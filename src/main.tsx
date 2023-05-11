3411ff5ada34bb8b6de2e6585fd69747be9ee67f
sebastian <sebas99cano@gmail.com>
2023-05-10 21:54:49 -0500

    se configura path para components, pages y assets. componente Home y se cambia el login por el home como punto de entrada

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Home from "@pages/home/Home";
 import "./assets/index.css";
-import Login from "./pages/login/components/Login.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Login />
+    <Home />
   </React.StrictMode>
 );
